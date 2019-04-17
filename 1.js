667a582a6d88250e1183615e8be577b787734966
Lan Nguyen <zhuylanz@gmail.com>
2019-04-17 21:02:19 +0700

    update result

--- 1.js
+++ 1.js
@@ -1,6 +1,6 @@
 var fs = require("fs");
 
-var file = fs.readFileSync("./hotel-california.txt", "utf8");
+var fileContent = fs.readFileSync("./hotel-california.txt", "utf8");
 
 // read file:
 console.log(file); // file is a String
@@ -12,19 +12,19 @@ for (var i = 0; i < alphabet.length; i++) {
 	var count = 0;
 
 	for (var j = 0; j < file.length; j++) {
-		if (file[j] === character) {
+		if (fileContent[j] === character) {
 			count++;
 		}
 	}
 
-	console.log("> character " + character + " : " + count + " | " + Math.round((count * 100) / file.length) + "%");
+	console.log("> character " + character + " : " + count + " | " + Math.round((count * 100) / fileContent.length) + "%");
 }
 
 // extracting data:
-var file2 = fs.readFileSync('./facebook-post.txt', 'utf8');
+var fileContent2 = fs.readFileSync('./facebook-post.txt', 'utf8');
 
-var email = file2.match(/\w+@\w+\.\w+/g)
-var phone = file2.match(/\d{10}/g)
+var email = fileContent2.match(/\w+@\w+\.\w+/g)
+var phone = fileContent2.match(/\d{10}/g)
 // console.log(email)
 // console.log(phone)
 // think: how to find address?
---
+++ excercises/ex1/ans.js
@@ -0,0 +1,34 @@
+var fs = require("fs");
+
+var listFiles = fs.readdirSync("./");
+var finalEmails = [];
+var finalPhones = [];
+var outputFileContent = "";
+
+for (var i = 0; i < listFiles.length; i++) {
+	var fileName = listFiles[i];
+	var fileContent = fs.readFileSync("./" + fileName, "utf8");
+	var email = fileContent.match(/\w+@\w+\.\w+/g);
+	var phone = fileContent.match(/\d{10}/g);
+	finalEmails.push(email);
+	finalPhones.push(phone);
+}
+
+// beautify email output
+outputFileContent += "Emails: \n";
+finalEmails.forEach(function(email) {
+	if (email) {
+		outputFileContent += email + "\n";
+	}
+});
+outputFileContent += "\n";
+
+// beautify phone output:
+outputFileContent += "Phones: \n";
+finalPhones.forEach(function(phone) {
+	if (phone) {
+		outputFileContent += phone + "\n";
+	}
+});
+
+fs.writeFileSync("./output.txt", outputFileContent);
